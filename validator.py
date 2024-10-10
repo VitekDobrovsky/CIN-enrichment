e4abd7f4a301a31606867a7360c91e96f1af595e
Vít Dobrovský <vit.dobrovsky@seznam.cz>
2024-10-10 07:56:52 +0200

    clean up

--- validator.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# validating ičo's using ares api
-
-response = requests.get("https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/27468801")
-
-
-print(response.json())
